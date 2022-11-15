e70819734bf8f1a097f67254aa38c6aba5506f58
Kharis <hh_shamsuarov@bingo-boom.com>
2022-11-15 23:36:33 +0300

    BTFS Stat

--- main.js
+++ main.js
@@ -2,10 +2,14 @@ import {ethers} from "ethers";
 const provider = new ethers.providers.JsonRpcProvider('https://rpc.bt.io');
 
 // Тут меняем на свои адреса
-const wallets = `0x0B6dE2FB2873cbf6220784BDB70F9d4388553621
-0x01d20d116c45634a3ae51f134804273eb466c5ca
-0x73568e787b9a0aa52c417536615ed647f8f5d895
-0xaac40485171774a08a9c326d584e877ecf44fe87`;
+const wallets = `
+
+0x0B6dE2FB2873cbf6220784BDB70F9d4388553621
+0x37cc0b9601ab42927d7a12aeef8d0a473c4f7642
+0x6D2D71B1d04bBA4A3E9B834bE5d7E7445eFDE36A
+0xB7dc0112FF1f6859daC30EC0e52e0711EdCc9075
+
+`;
 
 // При необходимости пополнить балансы:
 // 1. Укажите приватный ключ кошелька откуда производить перевод
