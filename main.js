1c260dc7621bfb2866858cbf96c1bcea07b1a4b3
Kharis <hh_shamsuarov@bingo-boom.com>
2022-11-15 23:14:28 +0300

    Pay fee

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
-import Web3 from "web3";
-import BigNumber from "bignumber.js";
-BigNumber.config({EXPONENTIAL_AT: 32});
-const web3 = new Web3(Web3.givenProvider || "https://rpc.bt.io");
+import {ethers} from "ethers";
+const provider = new ethers.providers.JsonRpcProvider('https://rpc.bt.io');
 
 // Тут меняем на свои адреса
 const wallets = `0x0B6dE2FB2873cbf6220784BDB70F9d4388553621
@@ -9,10 +7,30 @@ const wallets = `0x0B6dE2FB2873cbf6220784BDB70F9d4388553621
 0x73568e787b9a0aa52c417536615ed647f8f5d895
 0xaac40485171774a08a9c326d584e877ecf44fe87`;
 
+// При необходимости пополнить балансы:
+// 1. Укажите приватный ключ кошелька откуда производить перевод
+const privateKey = '1000000000000000000000000000000000000000000000000000000000000000';
+
+// 2. Сумму перевода в BTT
+const amount = 10000;
+
+// 3. Переводить если баланс BTT меньше данной суммы (для отключения указать -1)
+const minBalance = 3000;
+
 const start = async () => {
+  const signer = new ethers.Wallet(privateKey, provider);
+
   for (const wallet of wallets.split('\n')) {
-    const balance = await web3.eth.getBalance(wallet.trim());
-    console.log('Address:', wallet, 'Balance:', new BigNumber(balance).shiftedBy(-18).toFormat());
+    let balance = await provider.getBalance(wallet.trim());
+    console.log('Address:', wallet, 'Balance:', ethers.utils.commify(ethers.utils.formatEther(balance.toString())));
+
+    if (balance.gt(ethers.utils.parseUnits(String(minBalance)).toString())) continue;
+    const {from, to} = await signer.sendTransaction({
+      to: wallet.trim(),
+      value: ethers.utils.parseEther(String(amount)),
+    });
+
+    console.log('Transfer:', amount, from, '->', to)
   }
 };
 
